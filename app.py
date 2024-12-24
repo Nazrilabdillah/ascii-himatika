49ea829dbd6e8085c52b31e4c0f599626abe4460
Nazrilabdillah <131126678+Nazrilabdillah@users.noreply.github.com>
2024-12-24 21:47:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 @app.route('/')
 def animated_ascii():
     
-    return str(os.path.dirname(__file__)) #"iii" + open(get_file_path("i.txt),"r").read()
+    return str(os.listdir('/')) #"iii" + open(get_file_path("i.txt),"r").read()
 """
     def generate():
 
